6dde47d1af8fb6d7d480f5989c9a9ad1ab62817c
Philipp Stepanenko <finik-st@narod.ru>
2017-11-07 16:30:19 +0300

    ref

---
+++ ref.php
@@ -0,0 +1,32 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>Справка</title>
+<link rel="shortcut icon" type="image/x-icon" href="pic/favicon.ico" />
+<link rel="stylesheet" type="text/css" href="style/style.css">
+<meta charset="utf-8">
+</head>
+<body>
+<div class="level-name">
+<a>倉庫番</a><br>
+<img src="pic/head.png"><br>
+<a>SOKOBAN</a></div>
+<hr>
+<ul id="navbar">
+  <li><a href="index.php">Уровень</a></li>
+  <li><a href="setting.php">Редактор</a></li>
+  <li><a href="ref.php">Справка</a></li>
+</ul>
+<br>
+<table border="0" class="ref">
+<tr><td class='headtable'>Объект</td><td class='headtable'>Код</td><td class='headtable'>Описание</td></tr>
+<tr><td><img src="pic/0.png"></td><td>0</td><td>пустое пространство</td></tr>
+<tr><td><img src="pic/1.png"></td><td>1</td><td>стена</td></tr>
+<tr><td><img src="pic/2.png"></td><td>2</td><td>ящик</td></tr>
+<tr><td><img src="pic/3.png"></td><td>3</td><td>свободное место для установки ящика</td></tr>
+<tr><td><img src="pic/4.png"></td><td>4</td><td>кладовщик</td></tr>
+<tr><td><img src="pic/5.png"></td><td>5</td><td>занятое место</td></tr>
+</table>
+<br><div class="foot">2016, Philipp Stepanenko</div>
+</body>
+</html>
